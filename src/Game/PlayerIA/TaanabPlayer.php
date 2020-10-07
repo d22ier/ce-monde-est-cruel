7af000131d810f8be07977bd0f5a9c05644f1d33
Didier Muhire <drezling@gmail.com>
2020-10-07 11:26:28 -0700

    first

--- src/Game/PlayerIA/TaanabPlayer.php
+++ src/Game/PlayerIA/TaanabPlayer.php
@@ -7,42 +7,32 @@ use Hackathon\Game\Result;
 /**
  * Class TaanabPlayers
  * @package Hackathon\PlayerIA
- * @author YOUR NAME HERE
+ * @author Didier Muhire
  */
 class TaanabPlayer extends Player
 {
-    protected $mySide;
-    protected $opponentSide;
-    protected $result;
+	protected $mySide;
+	protected $opponentSide;
+	protected $result;
 
-    public function getChoice()
-    {
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Choice           ?    $this->result->getLastChoiceFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Choice ?    $this->result->getLastChoiceFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide) -- if 0 (first round)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide) -- if 0 (first round)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get all the Choices          ?    $this->result->getChoicesFor($this->mySide)
-        // How to get the opponent Last Choice ?    $this->result->getChoicesFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get my Last Score            ?    $this->result->getLastScoreFor($this->mySide)
-        // How to get the opponent Last Score  ?    $this->result->getLastScoreFor($this->opponentSide)
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the stats                ?    $this->result->getStats()
-        // How to get the stats for me         ?    $this->result->getStatsFor($this->mySide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // How to get the stats for the oppo   ?    $this->result->getStatsFor($this->opponentSide)
-        //          array('name' => value, 'score' => value, 'friend' => value, 'foe' => value
-        // -------------------------------------    -----------------------------------------------------
-        // How to get the number of round      ?    $this->result->getNbRound()
-        // -------------------------------------    -----------------------------------------------------
-        // How can i display the result of each round ? $this->prettyDisplay()
-        // -------------------------------------    -----------------------------------------------------
-
-
-        return parent::rockChoice();
-
-    }
+	public function getChoice()
+	{
+		$turn = $this->result->getNbRound();
+		$c =  parent::scissorsChoice();
+		$f =  parent::paperChoice();
+		$p = parent::rockChoice();
+		for ($i = 1; $i < $turn; $i++) {
+			$my_move = $this->result->getLastChoiceFor($this->mySide);
+			$his_move =  $this->result->getLastChoiceFor($this->opponentSide);
+			if ($his_move == $c)
+			{
+				return $f;
+			}else if ($his_move == $p){
+				return $f;
+			} else if ($his_move == $f){
+				return $c;
+			}
+		}
+		return $p;
+	}
 };
